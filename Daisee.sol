d740ba66bc08a7503cbf6b50ba5082a5fb515d8a
xalava <xavier.lavayssiere@gmail.com>
2016-05-22 15:23:24 +0200

    ParFraction

--- Daisee.sol
+++ Daisee.sol
@@ -4,7 +4,7 @@ contract Daisee {
     uint public currentTime;
     uint prixKwh;
 
-    /* Contributeurs */
+    /* Usagers */
     struct Usager {
         address addr;
         uint[] conso;
@@ -15,6 +15,8 @@ contract Daisee {
         uint credit;
     }
 
+    Usager anusager;
+
     /*  Initialisation */
     function Daisee() {
         currentTime = 0;
@@ -24,7 +26,7 @@ contract Daisee {
     function () {
         bool trouve=false;
 
-        for (uint i = 0; i < contributors.length; ++i) {
+        for (uint i = 0; i < usagers.length; ++i) {
             if(usagers[i].addr == msg.sender){
                 usagers[i].credit = usagers[i].credit + msg.value;
                 trouve=true;
@@ -41,8 +43,15 @@ contract Daisee {
     function sajouterUsager(
         string name
         ) {
+        Usager nouvelUsager = anusager;
+        nouvelUsager.nom = name;
+        nouvelUsager.credit = msg.value;
+        nouvelUsager.addr = msg.sender;
 
-        usagers[usagers.length++] = Usager({addr: msg.sender, credit: msg.value, nom: name});
+
+        usagers[usagers.length++] = nouvelUsager;
+
+        // {addr: msg.sender, credit: msg.value, nom: name, conso:[0], prod: [0], disponible:[0]}
         for (uint i = 0; i < currentTime; ++i) {
             usagers[usagers.length].conso[i]=0;  
             usagers[usagers.length].prod[i]=0;
@@ -51,9 +60,9 @@ contract Daisee {
     }
 
     function publier(uint consoActuelle, uint prodActuelle) {
-        prodDiff = prodActuelle - consoActuelle;
-        address vendeur;
-        bool trouve;
+        uint prodDiff = prodActuelle - consoActuelle;
+        // address vendeur;
+        bool termine;
 
         for (uint i = 0; i < usagers.length; ++i) {
             if(usagers[i].addr == msg.sender){
@@ -64,28 +73,38 @@ contract Daisee {
                     usagers[i].disponible[currentTime] = prodDiff;
                 } else {
                     for (uint j = 0; j < usagers.length; ++j) {
-                        if(usagers[j].disponible[currentTime-1]>= prodDiff){
-                            vendeur = usagers[j].addr;
-                            trouve = true;
+                        if(usagers[j].disponible[currentTime-1]> 0){
+                            // vendeur = usagers[j].addr;
+                            uint dispo;
+
+                            if (prodDiff > dispo){
+                                usagers[j].disponible[currentTime-1] = 0;
+                                usagers[j].credit +=  dispo * prixKwh;
+                                usagers[i].credit -=  dispo * prixKwh;
+                                prodDiff -= dispo;
+                            }else {
+                                usagers[j].disponible[currentTime-1] -= prodDiff;
+                                usagers[j].credit +=  prodDiff * prixKwh;
+                                usagers[i].credit -=  prodDiff * prixKwh;
+
+                                termine = true;
+                                return;
+                            }
                         }
                     }
-                    if(trouve){
-                        usagers[j].disponible[currentTime-1] -= prodDiff;
-                        usagers[j].credit +=  prodDiff * prixKwh;
-                        usagers[i].credit -=  prodDiff * prixKwh;
+                    if(termine){
 
                     }else{
-                        /*Acheter hors reseau ou tenter achat par fractions*/
+                        /*Acheter hors reseau */
                     }
 
-
                 }
-
             }
         } 
+    }
 
-
-
+    function nextTime() {
+        currentTime++; 
     }
 
 }
